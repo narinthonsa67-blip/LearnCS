d47c8e442b427e8d3b1419950f860404df2cadca
narinthonsa67-blip <narinthon.sa.67@ubu.ac.th>
2026-06-17 10:20:49 +0700

    Learning arrays

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.ExceptionServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,6 +12,22 @@ namespace LearnCS
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            int a = 60, b = 7;
+            Console.WriteLine($"A + B = {a + b}");
+            // Addition
+            /* WASD
+             * WASD
+             * WASD
+             */
+            // Arrays
+            string[] cars = new string[3];
+            cars[0] = "Ford";
+            cars[1] = "BMW";
+            cars[2] = "Toyota";
+            Console.WriteLine($"First car: {cars[0]}");
+            Console.WriteLine($"Second car: {cars[1]}");
+            Console.WriteLine($"Third car: {cars[2]}");
+
         }
     }
 }
